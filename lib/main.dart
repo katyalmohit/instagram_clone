4fc632d6e84227714d54ba3186f38fb0412b0050
katyalmohit <128162892+katyalmohit@users.noreply.github.com>
2024-07-07 22:39:36 +0530

    Create login and signup page

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:instagram_clone/responsive/mobile_screen_layout.dart';
 import 'package:instagram_clone/responsive/responsive_layout_screen.dart';
 import 'package:instagram_clone/responsive/web_screen_layout.dart';
+import 'package:instagram_clone/screens/login_screen.dart';
+import 'package:instagram_clone/screens/signup_screen.dart';
 import 'package:instagram_clone/utils/colors.dart';
 import 'package:firebase_core/firebase_core.dart';
 
@@ -37,10 +39,11 @@ class MyApp extends StatelessWidget {
       theme: ThemeData.dark().copyWith(
         scaffoldBackgroundColor: mobileBackgroundColor,
       ),
-      home: ResponsiveLayout(
-        mobileScreenLayout: MobileScreenLayout(),
-        webScreenLayout: WebScreenLayout(),
-      ),
+      // home: ResponsiveLayout(
+      //   mobileScreenLayout: MobileScreenLayout(),
+      //   webScreenLayout: WebScreenLayout(),
+      // ),
+      home:SignupScreen(),
     );
   }
 }
---
+++ lib/screens/signup_screen.dart
@@ -0,0 +1,165 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:instagram_clone/utils/colors.dart';
+import 'package:instagram_clone/widgets/text_field_input.dart';
+
+class SignupScreen extends StatefulWidget {
+  const SignupScreen({super.key});
+
+  @override
+  State<SignupScreen> createState() => SignupScreenState();
+}
+
+class SignupScreenState extends State<SignupScreen> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _bioController = TextEditingController();
+  final TextEditingController _usernameController = TextEditingController();
+
+  @override
+  void dispose() {
+    super.dispose();
+    _emailController.dispose();
+    _passwordController.dispose();
+    _bioController.dispose();
+    _usernameController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: SafeArea(
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 32),
+        width: double.infinity,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Flexible(child: Container(), flex: 1),
+            //svg image
+            SvgPicture.asset(
+              'assets/ic_instagram.svg',
+              colorFilter: ColorFilter.mode(primaryColor, BlendMode.srcIn),
+              height: 64,
+            ),
+            const SizedBox(
+              height: 64,
+            ),
+
+            //circular widget to accept and show our selected files
+            Stack(
+              children: [
+                const CircleAvatar(
+                  radius: 64,
+                  backgroundImage: NetworkImage(
+                      'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/42d4d4c5-7c8d-4a7a-85d3-d639390fda35/dfgjtob-2fce0d48-8173-4476-8d65-55dfa73b358f.png/v1/fill/w_894,h_894,q_70,strp/lord_inosuke_coming_through__by_ndnhan_dfgjtob-pre.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTI4MCIsInBhdGgiOiJcL2ZcLzQyZDRkNGM1LTdjOGQtNGE3YS04NWQzLWQ2MzkzOTBmZGEzNVwvZGZnanRvYi0yZmNlMGQ0OC04MTczLTQ0NzYtOGQ2NS01NWRmYTczYjM1OGYucG5nIiwid2lkdGgiOiI8PTEyODAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.hVxySQNAxPCj5abYFSylBCTDxrHP4HsLKJd7pXNXrfE'),
+                ),
+                Positioned(
+                  bottom: -10,
+                  left:80,
+                  child: IconButton(
+                    onPressed: () {},
+                    icon: const Icon(Icons.add_a_photo),
+                  ),
+                )
+              ],
+            ),
+
+            const SizedBox(
+              height: 24,
+            ),
+
+            //text field input for username
+            TextFieldInput(
+              hintText: "Enter your username",
+              textInputType: TextInputType.text,
+              textEditingController: _usernameController,
+              isPass: true,
+            ),
+
+            const SizedBox(
+              height: 24,
+            ),
+
+            //text field input for email
+            TextFieldInput(
+              hintText: "Enter your email",
+              textInputType: TextInputType.emailAddress,
+              textEditingController: _emailController,
+            ),
+
+            const SizedBox(
+              height: 24,
+            ),
+
+            //text field input for password
+            TextFieldInput(
+              hintText: "Enter your password",
+              textInputType: TextInputType.text,
+              textEditingController: _passwordController,
+              isPass: true,
+            ),
+
+            const SizedBox(
+              height: 24,
+            ),
+
+            //text field input for bio controller
+            TextFieldInput(
+              hintText: "Enter your bio",
+              textInputType: TextInputType.text,
+              textEditingController: _bioController,
+              isPass: true,
+            ),
+
+            const SizedBox(
+              height: 24,
+            ),
+            //button login
+            InkWell(
+              onTap: () {},
+              child: Container(
+                child: const Text("Log in"),
+                width: double.infinity,
+                alignment: Alignment.center,
+                padding: const EdgeInsets.symmetric(vertical: 12),
+                decoration: const ShapeDecoration(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(4),
+                    ),
+                  ),
+                  color: blueColor,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 12,
+            ),
+            Flexible(child: Container(), flex: 2),
+            //transitioning to signing up
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  child: const Text("Don't have an account?"),
+                  padding: const EdgeInsets.symmetric(vertical: 8),
+                ),
+                GestureDetector(
+                  onTap: () {},
+                  child: Container(
+                    child: const Text(
+                      "Sign up",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    padding: const EdgeInsets.symmetric(vertical: 8),
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    ));
+  }
+}
