c5d32f5a38730ba9270e87f2696a2b5cbbc0c7b6
katyalmohit <128162892+katyalmohit@users.noreply.github.com>
2024-07-11 20:56:35 +0530

    Make responsive UI for web

--- lib/screens/feed_screen.dart
+++ lib/screens/feed_screen.dart
@@ -2,15 +2,23 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:instagram_clone/utils/colors.dart';
+import 'package:instagram_clone/utils/global_variables.dart';
 import 'package:instagram_clone/widgets/post_card.dart';
 
-class FeedScreen extends StatelessWidget {
+class FeedScreen extends StatefulWidget {
   const FeedScreen({Key? key}) : super(key: key);
 
   @override
+  State<FeedScreen> createState() => _FeedScreenState();
+}
+
+class _FeedScreenState extends State<FeedScreen> {
+  @override
   Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
     return Scaffold(
-      appBar: AppBar(
+      backgroundColor:width > webScreenSize? webBackgroundColor: mobileBackgroundColor,
+      appBar:width>webScreenSize? null : AppBar(
         backgroundColor: mobileBackgroundColor,
         centerTitle: false,
         title: SvgPicture.asset('assets/ic_instagram.svg',
@@ -33,8 +41,14 @@ class FeedScreen extends StatelessWidget {
           }
           return ListView.builder(
             itemCount: snapshot.data!.docs.length,
-            itemBuilder: (context, index) => PostCard(
+            itemBuilder: (context, index) => Container(margin: EdgeInsets.symmetric(
+              horizontal: width > webScreenSize? width*0.3 : 0,
+              vertical: width > webScreenSize ? 15:0,
+            ),
+            
+            child: PostCard(
               snap: snapshot.data!.docs[index].data(),
+            ),
             ),
           );
         },
